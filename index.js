eab11cb94586724764224fe1b31bc5a063eba782
Ehsan <l181092@lhr.nu.edu.pk>
2022-05-31 18:52:44 +0500

    Trying to upload

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ app.use(expressModule.json());
 const Joi = require("joi");
 
 const cors = require("cors");
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.options("/getimages", cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
